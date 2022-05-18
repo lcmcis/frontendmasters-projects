f8113c90349b986121907822e78495c88f0413c1
lcmcis <lcmcis21@gmail.com>
2022-05-18 15:30:37 -0700

    commit

---
+++ objects/object1.js
@@ -0,0 +1,13 @@
+const person = {
+    name: "John Doe",
+    city: "Charleston",
+    state: "SC",
+    favoriteCity: "Tahoe City",
+    favoriteFruit: "Mango",
+    likesMango: true,
+    numberOfMangoesWanted: 20
+}
+
+console.log(person)
+console.log(person.city)
+console.log(person["favoriteCity"])
